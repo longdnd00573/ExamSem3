cc2de1d4e67c3dfe311ba7786941d10ae63b61ff
longdnd00573 <longdnd00573@fpt.edu.vn>
2018-12-04 10:19:42 +0700

    nop bai

---
+++ Exam-sem3/Controllers/ExamController.cs
@@ -0,0 +1,44 @@
+﻿using Exam_sem3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exam_sem3.Controllers
+{
+    public class ExamController : Controller
+    {
+        ExamWADEntities db = new ExamWADEntities();
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Exam exam)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    exam.Status = 1;
+                    db.Exams.Add(exam);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(exam);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Exam-sem3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exam_sem3.MvcApplication" Language="C#" %>
